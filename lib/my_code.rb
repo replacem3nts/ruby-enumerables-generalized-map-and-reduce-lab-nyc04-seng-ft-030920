19f35ba45e3a1da4090edbefe1b4ea2665d998f3
Donny L <donny.landis@gmail.com>
2020-02-21 03:44:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,19 +8,41 @@ def map(array)
   new
 end
 
+#def reduce(array, start = 0)
+#  total = 0
+#  i = 0
+#    while i < array.length do
+#      if array[i].class == Integer
+#        total += array[i]
+#      elsif !array[i]
+#        total = false
+#      else
+#        start = true
+#        total = true
+#      end
+#      i += 1
+#    end
+#  pp yield(start, total)
+#end
+
 def reduce(array, start = 0)
-  total = 0
+  total = start
   i = 0
-    while i < array.length do
-      if array[i].class == Integer
-        total += array[i]
-      elsif !array[i]
-        total = false
-      else
-        start = true
-        total = true
-      end
-      i += 1
-    end
-  pp yield(start, total)
+  while i < array.length do
+    total = yield(total, array[i])
+    i += 1
+  end
+ pp total
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
