c8b92790d8586fb8ad061357848d05b0e52b43ac
Donny L <donny.landis@gmail.com>
2020-02-21 02:43:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << yield(array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(array, start = 0)
+  total = 0
+  i = 0
+    while i < array.length do
+      if array[i].class == Integer
+        total += array[i]
+      elsif !array[i]
+        total = false
+      else
+        start = true
+        total = true
+      end
+      i += 1
+    end
+  pp yield(start, total)
+end
